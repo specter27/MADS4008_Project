fed163683bc89963a0fc1fb04c95f0c1566cf68a
h-sompura <sompharshi123@gmail.com>
2022-07-29 23:35:45 -0400

    Added Stack Navigation and Purchase LoggedOut userflow completed

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 // Navigation imports
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 // Screens imports
 import NowPlayingScreen from './screens/NowPlayingScreen';
 import MyPurchases from './screens/MyPurchases';
 import LogoutScreen from './screens/LogoutScreen';
+import LoginScreen from './screens/LoginScreen';
 // Vector Icons
 import Icon from 'react-native-vector-icons/FontAwesome';
 //Firebase Imports
@@ -14,12 +16,50 @@ import {auth} from "./FirebaseApp";
 // get the functions from the Firebase Auth library
 import {  onAuthStateChanged } from "firebase/auth";
 
+
+
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   // state variable to track if there is a logged in user
   const [userLoggedIn, setUserLoggedIn] = useState(false)
   
+  // ----------- Bottom tabs Component ---------
+  function BottomTabs (){
+    //returns the tab navigator to stack
+    return (
+      <Tab.Navigator screenOptions={{ 
+          "tabBarActiveTintColor": "#3330e3", 
+          "tabBarInactiveTintColor": "gray",
+         }}>
+           <Tab.Screen name="NowPlaying" component={NowPlayingScreen}
+               options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon name="list-ul" color={color} size={size} />
+                ),
+              }}
+           />
+           <Tab.Screen name="MyPurchases" component={MyPurchases}
+             options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon name="ticket" color={color} size={size} />
+                ),
+              }}
+           />
+           { (userLoggedIn === true )
+             // User has logged in so display another tab for Logout
+             && <Tab.Screen name="Logout" component={LogoutScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="user-o" color={color} size={size} />
+                  ),
+              }}/>
+            }
+        </Tab.Navigator>
+    );
+  }
+
   useEffect(()=>{
   // code to check if there is a logged in user
   onAuthStateChanged(auth, user => {
@@ -34,34 +74,13 @@ export default function App() {
 
   return (
     <NavigationContainer>
-       <Tab.Navigator screenOptions={{ 
-        "tabBarActiveTintColor": "#3330e3", 
-        "tabBarInactiveTintColor": "gray",
-       }}>
-         <Tab.Screen name="NowPlaying" component={NowPlayingScreen}
-             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Icon name="list-ul" color={color} size={size} />
-              ),
-            }}
-         />
-         <Tab.Screen name="MyPurchases" component={MyPurchases}
-           options={{
-              tabBarIcon: ({ color, size }) => (
-                <Icon name="ticket" color={color} size={size} />
-              ),
-            }}
-         />
-         { (userLoggedIn === true )
-           // User has logged in so display another tab for Logout
-           && <Tab.Screen name="Logout" component={LogoutScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="user-o" color={color} size={size} />
-                ),
-            }}/>
-          }
-       </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="BottomNavBar"
+            component={BottomTabs}
+            options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- screens/MyPurchases.js
+++ screens/MyPurchases.js
@@ -1,11 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, SafeAreaView, View, Pressable } from 'react-native';
+//Firebase Imports
+import {auth} from "../FirebaseApp";
+// get the functions from the Firebase Auth library
+import {  onAuthStateChanged } from "firebase/auth";
 
-const MyPurchases = () => {
+
+const MyPurchases = ({navigation}) => {
+
+   // -------------- State Variables -------------------
+   const [viewsToRender, setViewsToRender] = useState();
+
+   // -------------- Event Handlers -------------------
+   const loginPressed = () => {
+      console.log(`Login Button Pressed!`);
+      //navigate to login screen
+      navigation.navigate("Login");
+   }
+
+   // -------------- Use Effect -------------------
+   useEffect(()=>{
+      // code to check if there is a logged in user
+      onAuthStateChanged(auth, user => {
+        if (user) {
+          console.log(`Checking user status!`); 
+          //Logged in user so display tickets list
+        } else {
+          // logged out user so display login button
+          setViewsToRender(
+            <View style={{alignItems:"center"}}>
+               <Text> You must be logged in to view this feature.</Text>
+               <Pressable style={styles.loginButton} onPress={loginPressed}>
+                  <Text style={styles.buttonText}> Log in or Create New Account</Text>
+               </Pressable>
+            </View>
+          )
+        }
+      });
+   }, [])
+
+
   return (
-     <View>
-        <Text>My Purchases Screen!</Text>
-     </View>
+     <SafeAreaView style={styles.container}>
+        <Text style={styles.screenHeading}>Your Tickets</Text>
+        {viewsToRender}
+     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+   screenHeading : {
+      fontSize: 22,
+      marginTop:10,
+      fontWeight: "400",
+      marginBottom:10,
+    },
+    loginButton : {
+      backgroundColor: "#3330e3",
+      alignSelf:"stretch",
+      padding:16,
+      paddingHorizontal:30,
+      marginHorizontal:20,
+      marginVertical:20,
+      borderRadius:8,
+    },
+    buttonText : {
+      color:"white",
+      fontSize: 18,
+      fontWeight: "bold"
+    },  
+ });
 
 export default MyPurchases
